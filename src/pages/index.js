36617e711784f0bec459da924029d9e24e1a496a
Developer <Dev@jacobs-macbook-pro.home>
2019-12-13 13:29:18 +0100

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,51 @@
+import React from "react"
+import { Link } from "gatsby"
+import axios from "Axios"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+
+
+function IndexPage() {
+
+  React.useEffect(() => {
+    fetchJSON();
+  })
+  
+  const fetchJSON = () => {
+    axios
+      .get(`http://doo.zone/api/method/auth.php?myusername=jakepeg@gmail.com&mypassword=worm1`)
+      .then(res => {
+         console.log(res.data);
+      })
+      .catch(error => {
+        console.log(`error = ${error}`);
+      });
+  };
+
+
+// async function fetchJSON() {
+//   const response = await fetch(`http://doo.zone/api/method/auth.php?myusername=jakepeg@gmail.com&mypassword=worm1`);
+//   const data = await response.json();
+//   let myChildren = data.response;
+//   console.log(myChildren);
+// }
+
+  return (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+  );
+}
+
+
+export default IndexPage
