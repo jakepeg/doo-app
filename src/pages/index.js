0a4261523f17a319cbfb362504a5b0494a8961d7
Developer <Dev@jacobs-macbook-pro.home>
2019-12-15 15:11:25 +0100

    put api on snorm.co

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ function IndexPage() {
   
   const fetchJSON = () => {
     axios
-      .get(`https://doo.zone/api/method/auth.php?myusername=jakepeg@gmail.com&mypassword=worm1`)
+      .get(`https://snorm.co/api/method/auth.php?myusername=jakepeg@gmail.com&mypassword=worm1`)
       .then(res => {
          console.log(res.data);
       })
